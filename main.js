dcad192dcdb4612c01d1c01a6b8a83ac4ac900f2
Roobee <ruthiebee@ruthies-mbp.home>
2019-04-08 01:46:34 +0100

    Added IF and Range functions to javascript for quiz page

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function check(){
 
-var question1 = document.quiz.question1.value;
-var question2 = document.quiz.question2.value;
-var question3 = document.quiz.question3.value;
-var question4 = document.quiz.question4.value;
+var question1 = document.quizz.question1.value;
+var question2 = document.quizz.question2.value;
+var question3 = document.quizz.question3.value;
+var question4 = document.quizz.question4.value;
 var correct = 0;
 
     if (question1=="Mojito") {
@@ -19,6 +19,32 @@ var correct = 0;
       correct++;
     }
 
+var messages = ["You're a quiz-o-holic!", "Just one more!","You like your marks how you like your drinks...half full!", "One is truly the lonliest number", "Paying more attention to your drink and not this quiz!"]
+
+var range;
+
+  if (correct == 0) {
+    range = 4;
+  }
+
+  if (correct == 1) {
+    range = 3
+  }
+
+  if (correct == 2) {
+    range = 2
+  }
+
+  if (correct == 3) {
+    range = 1
+  }
+
+  if (correct == 4) {
+    range = 0
+  }
+
   document.getElementById("after_submit").style.visibility="visible";
+
+  document.getElementById("message").innerHTML= messages[range];
   document.getElementById("clicked_correct").innerHTML="You got " + correct + " correct!";
   }
